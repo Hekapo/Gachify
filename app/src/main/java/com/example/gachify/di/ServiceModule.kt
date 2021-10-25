9422267cd0a2c78d66839288d7f8c60fb8adbca0
Iskandar <iskandar200213@gmail.com>
2021-10-25 10:32:55 +0300

    added glide module

---
+++ app/src/main/java/com/example/gachify/di/ServiceModule.kt
@@ -0,0 +1,13 @@
+package com.example.gachify.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+
+
+
+}
