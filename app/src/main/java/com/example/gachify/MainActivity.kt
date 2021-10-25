fd2144504e008e04840a28519030cd11a5dda950
Iskandar <iskandar200213@gmail.com>
2021-10-25 12:11:30 +0300

    added service module

--- app/src/main/java/com/example/gachify/MainActivity.kt
+++ app/src/main/java/com/example/gachify/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.gachify
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.example.gachify.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @Inject
+    lateinit var glide: RequestManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
